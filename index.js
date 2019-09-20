a70507ea8d438b49773f544d1d929f6146bcbc82
HuiloIvanovich <alex4152001@mail.ru>
2019-09-20 22:00:18 +0300

    fixed start scripts

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+console.log('test');
 const express = require('express');
 const connectDB = require('./config/db');
+
+
 
 const app = express();
 
 app.use(express.json({extended: false}));
 
-connectDB();
+//connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (req, res) => {
     //console.log(`Server started on port ${PORT}`);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Blank page\n');
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.end('Blank page\n');
     console.log('Hello world');
 });
