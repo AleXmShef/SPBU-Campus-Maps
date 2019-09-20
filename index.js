00c685a21d15cbd50f1fbccba558d6665437c5fc
HuiloIvanovich <alex4152001@mail.ru>
2019-09-20 21:48:44 +0300

    fixed server response

--- index.js
+++ index.js
@@ -11,5 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     //console.log(`Server started on port ${PORT}`);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Blank page\n');
     console.log('Hello world');
 });
