cb668f6c96d1c7862eb9c6d451293c648de7eede
HuiloIvanovich <alex4152001@mail.ru>
2019-09-20 22:14:34 +0300

    react build on heroku

--- index.js
+++ index.js
@@ -12,9 +12,21 @@ app.use(express.json({extended: false}));
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+else {
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    });
+}
 
 app.listen(PORT, () => {
     console.log('Hello world');
