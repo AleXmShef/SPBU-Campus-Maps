4b664ecb52329ba58b289c354c7168fc3e8eeb0b
HuiloIvanovich <alex4152001@mail.ru>
2019-09-20 22:40:28 +0300

    react build on heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 console.log('test');
 const express = require('express');
 const connectDB = require('./config/db');
+const qs = require('querystring');
+const crypto = require('crypto');
 
 
 
@@ -8,7 +10,7 @@ const app = express();
 
 app.use(express.json({extended: false}));
 
-//connectDB();
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
@@ -18,13 +20,51 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        const urlParams = req.params;
+        const ordered = {};
+        Object.keys(urlParams).sort().forEach((key) => {
+            if (key.slice(0, 3) === 'vk_') {
+                ordered[key] = urlParams[key];
+            }
+        });
+
+        const stringParams = qs.stringify(ordered);
+        const paramsHash = crypto
+            .createHmac('sha256', secretKey)
+            .update(stringParams)
+            .digest()
+            .toString('base64')
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_')
+            .replace(/=$/, '');
+        console.log(paramsHash);
+        res.json(paramsHash);
+        //res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
 else {
     app.get('/', (req, res) => {
-        res.send('Hello World!')
+        //res.send('Hello World!')
+        const urlParams = req.params;
+        const ordered = {};
+        Object.keys(urlParams).sort().forEach((key) => {
+            if (key.slice(0, 3) === 'vk_') {
+                ordered[key] = urlParams[key];
+            }
+        });
+
+        const stringParams = qs.stringify(ordered);
+        const paramsHash = crypto
+            .createHmac('sha256', secretKey)
+            .update(stringParams)
+            .digest()
+            .toString('base64')
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_')
+            .replace(/=$/, '');
+        console.log(paramsHash);
+        res.json(paramsHash);
     });
 }
 
